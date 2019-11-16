503d0a5ba9eb582a76f7094ed271d3caeda29aff
Vitao18 <victoronline9@gmail.com>
2019-11-16 18:47:40 -0300

    complete app xgh commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Button, View, FlatList } from "react-native";
 
-export default function App() {
+const getRandomInt = (min, max) => () => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+};
+
+const getRandomIntTo256 = getRandomInt(0, 256);
+
+const generateRandomRgb = () =>
+  `rgb(${getRandomIntTo256()}, ${getRandomIntTo256()}, ${getRandomIntTo256()})`;
+
+const renderItem = ({ item }) => (
+  <View key={item} style={{ height: 100, width: 100, backgroundColor: item }} />
+);
+
+const App = () => {
+  const [colors, setColors] = useState<Array<string>>([]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title="ADD COLOR"
+        onPress={() => setColors(colors.concat(generateRandomRgb()))}
+      />
+      <FlatList
+        data={colors}
+        renderItem={renderItem}
+        keyExtractor={item => item}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    marginTop: 30
+  }
 });
+
+export default App;
